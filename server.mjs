a42201e6dcc0c90362bed4f653c86582d2f2bb0a
afzalahmed81 <afzalahmed123@gmail.com>
2022-10-19 12:05:31 +0500

    netlify url modified in server

--- server.mjs
+++ server.mjs
@@ -17,7 +17,7 @@ app.use(cookieParser()); // parsing cookies
 
 
 app.use(cors({
-    origin: ['http://localhost:3000','https://e-commerce-23cfd.web.app/login','https://634f9f6477f3d656dc01245a--calm-rabanadas-091d83.netlify.app/login',"*"],
+    origin: ['http://localhost:3000','https://e-commerce-23cfd.web.app','https://634f9f6477f3d656dc01245a--calm-rabanadas-091d83.netlify.app',"*"],
     credentials: true
 }));
 
